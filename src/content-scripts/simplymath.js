4e1e3eeba490e0558c1264cb45c1eafe59667fa4
ryan <4721319+rpshukla@users.noreply.github.com>
2021-01-17 00:10:25 -0700

    Find editable div

--- src/content-scripts/simplymath.js
+++ src/content-scripts/simplymath.js
@@ -1,19 +1,11 @@
-// untested
-function getElementCursorIsIn() {
-  const selection = window.getSelection();
-  if (selection === undefined || selection === null) {
-    console.log("selection null");
-    return null;
-  }
-  return selection.anchorNode;
-}
-
-async function writeClipImg(url) {
+async function writeImageToClipboard(url) {
   try {
     // How to copy image to clipboard
     // https://gist.github.com/dvreed77/c37759991b0723eebef3647015495253
-    const image = new Image();
-    image.src = url;
+    //const image = new Image();
+    //image.src = url;
+    const image = document.createElement('p');
+    image.innerHTML = 'hilo';
     document.body.appendChild(image);
     const r = document.createRange();
     r.setStartBefore(image);
@@ -28,11 +20,53 @@ async function writeClipImg(url) {
   }
 }
 
+let currentEditableElement = null;
+let currentIframe = null;
+
+function pasteImageFromClipboard() {
+  console.log("pasteImageFromClipboard")
+  console.log(currentEditableElement);
+  if (currentEditableElement === null) {
+    return;
+  }
+  if (currentIframe !== null) {
+    let win = currentIframe.contentWindow;
+    let range = win.document.createRange();
+    range.setStart(win.document.body, 0);
+    range.setEnd(win.document.body, 0);
+    win.document.body.focus();
+    win.getSelection().addRange(range);
+  }
+  currentEditableElement.focus();
+  document.execCommand("paste");
+}
+
 window.addEventListener('load', () => {
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
     console.log(request);
     if (request.message === 'insertImage') {
-      writeClipImg(request.imageUrl);
+      await writeImageToClipboard(request.imageUrl);
+      pasteImageFromClipboard();
     }
   });
 });
+
+window.addEventListener('focusin', () => {
+  const activeElement = document.activeElement;
+  console.log("activeElement:");
+  console.log(activeElement);
+  if (activeElement.tagName === "IFRAME") {
+    console.log("iframe");
+    const editableElement = activeElement.contentWindow.document.querySelector('div[contenteditable="true"]');
+    if (editableElement === undefined || editableElement === null) {
+      return;
+    }
+    console.log("editableElement:");
+    console.log(editableElement);
+    currentEditableElement = editableElement;
+    currentIframe = activeElement;
+  } else {
+    currentEditableElement = activeElement;
+    currentIframe = null;
+  }
+})
