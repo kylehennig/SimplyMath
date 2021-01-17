523b91b5a8c7154437caa1ab849e8e2467018a89
Kyle Hennig <henn.kyle@outlook.com>
2021-01-16 19:53:00 -0700

    Send image to content scripts via message passing

--- src/browser-action/simplymath.js
+++ src/browser-action/simplymath.js
@@ -2,29 +2,27 @@ window.addEventListener('load', () => {
   const mathQuill = MathQuill.getInterface(2);
   const mathQuillInput = document.getElementById('mathquill-input');
   const latexOutput = document.getElementById('latex-output');
-  const imageOutput = document.getElementById('image-output');
   const mathField = mathQuill.MathField(mathQuillInput, {
     handlers: {
-      edit: async () => {
+      edit: () => {
         const enteredMath = mathField.latex();
         latexOutput.textContent = enteredMath;
-        try {
-          const equationElement = mathQuillInput.querySelector('.mq-root-block');
-          const cursorElement = equationElement.querySelector('.mq-cursor');
-          cursorElement.style.visibility = 'hidden';
-          const dataUrl = await domtoimage.toPng(equationElement);
-          cursorElement.style.visibility = 'visible';
-          imageOutput.src = dataUrl;
-        } catch (error) {
-          console.error("An error occured while converting the equation to a PNG: " + e);
-        }
-
-        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-          chrome.tabs.sendMessage(tabs[0].id, { message: "Hello!" }, response => {
-            console.log(response);
-          })
-        });
       }
+    }
+  });
+
+  const insertButton = document.getElementById('insert-button');
+  insertButton.addEventListener('click', async () => {
+    try {
+      const equationElement = mathQuillInput.querySelector('.mq-root-block');
+      const dataUrl = await domtoimage.toPng(equationElement);
+      chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        chrome.tabs.sendMessage(tabs[0].id, { message: 'insertImage', imageUrl: dataUrl }, response => {
+          console.log(response);
+        })
+      });
+    } catch (error) {
+      console.error('An error occured while converting the equation to a PNG: ' + error);
     }
   });
 });
