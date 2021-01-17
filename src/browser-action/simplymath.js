9d92531ca357dd46d7c43c40d9c1e708d4ffc621
Kyle Hennig <henn.kyle@outlook.com>
2021-01-16 17:14:32 -0700

    Preview the entered equation as LaTeX and a PNG image

--- src/browser-action/simplymath.js
+++ src/browser-action/simplymath.js
@@ -1,11 +1,19 @@
 window.addEventListener('load', () => {
   const mathQuill = MathQuill.getInterface(2);
   const mathQuillSpan = document.getElementById('mathquill-span');
+  const latexSpan = document.getElementById('latex-span');
+  const pngImage = document.getElementById('png-image');
   const mathField = mathQuill.MathField(mathQuillSpan, {
     handlers: {
-      edit: function () {
+      edit: async () => {
         const enteredMath = mathField.latex();
-        console.log(enteredMath);
+        latexSpan.textContent = enteredMath;
+        try {
+          const dataUrl = await domtoimage.toPng(mathQuillSpan);
+          pngImage.src = dataUrl;
+        } catch (error) {
+          console.error("An error occured while converting the equation to a PNG: " + e);
+        }
       }
     }
   });
