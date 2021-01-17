bcabb92fc6c941d85626df19f0a436cfafe3e7f1
ryan <4721319+rpshukla@users.noreply.github.com>
2021-01-17 09:51:34 -0700

    Add alt text prefix and click handler

--- src/content-scripts/simplymath.js
+++ src/content-scripts/simplymath.js
@@ -1,11 +1,12 @@
 window.addEventListener('load', () => {
+  const altTextPrefix = '$latex$';
   const writeImageToClipboard = async (url, latex) => {
     try {
       // How to copy image to clipboard
       // https://gist.github.com/dvreed77/c37759991b0723eebef3647015495253
       const image = new Image();
       image.src = url;
-      image.alt = latex;
+      image.alt = altTextPrefix + latex;
       document.body.appendChild(image);
       const r = document.createRange();
       r.setStartBefore(image);
@@ -40,6 +41,40 @@ window.addEventListener('load', () => {
     }
   }
 
+  const clickHandler = (event) => {
+    // check if main mouse button was clicked
+    if (event.button !== 0) {
+      return;
+    }
+
+    const clickedElement = event.target;
+
+    const images = clickedElement.querySelectorAll('image');
+    images.forEach((image) => {
+      // check if click lands within image
+      const domRect = image.getBoundingClientRect();
+      if (event.userX < domRect.left) return;
+      if (event.userX > domRect.right) return;
+      if (event.userY < domRect.top) return;
+      if (event.userY > domRect.bottom) return;
+
+      // NOTE: this is google docs specific
+      // alt text is in desc tag
+      const desc = clickedElement.querySelector('desc');
+      if (desc === undefined || desc === null) {
+        return;
+      }
+
+      const altText = desc.textContent;
+      if (!altText.startsWith(altTextPrefix)) {
+        return;
+      }
+
+      const latex = altText.substr(altTextPrefix.length);
+      console.log(latex);
+    });
+  }
+
   window.addEventListener('focusin', () => {
     const activeElement = document.activeElement;
     if (activeElement.tagName === 'IFRAME') {
@@ -60,5 +95,9 @@ window.addEventListener('load', () => {
       await writeImageToClipboard(request.imageUrl, request.latex);
       pasteImageFromClipboard();
     }
+  });
+
+  window.addEventListener('click', (event) => {
+    clickHandler(event);
   });
 });
